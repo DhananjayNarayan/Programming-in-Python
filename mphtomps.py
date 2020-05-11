bc787098a8037852637d79df4dac58b2aa5c2372
Dhananjay Narayan <dhananjayashwath@gmail.com>
2020-05-11 23:54:45 +0530

    Simple Code to Convert MPH to MPS

--- mphtomps.py
+++ mphtomps.py
@@ -1,7 +1,7 @@
 print("MPH to MPS Conversion App")
 
  mph = float(input("\nWhat is the speed in miles per hour: "))
- #Convert to mps
+ #Convert to mph to mps. Where 1mph= 0.4474 mps
  mps = mph*0.4474
  mps = round(mps, 2)
  print("Your speed in meters per second is " + str(mps) + ".")
