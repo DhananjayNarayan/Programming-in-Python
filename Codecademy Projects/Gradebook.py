2d540180a16fb5693b5698903ede24a1b80ab495
Dhananjay Narayan <dhananjayashwath@gmail.com>
2020-05-23 02:21:38 +0530

    Create Gradebook.py

---
+++ Codecademy Projects/Gradebook.py
@@ -0,0 +1,21 @@
+"""
+Python Gradebook
+You are a student and you are trying to organize your subjects and grades using Python. 
+Explore what we’ve learned about lists to organize your subjects and scores.
+"""
+
+
+last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+subjects= ['physics', 'calculus', 'poetry', 'history']
+subjects.append('computer science')
+grades=[98, 97, 85, 88]
+grades.append(100)
+gradebook=zip(subjects,grades)
+gradebook=list(gradebook)
+gradebook.append(('visual arts', 93))
+#print(gradebook)
+full_gradebook= last_semester_gradebook+ gradebook
+print(full_gradebook)
+
+
+
