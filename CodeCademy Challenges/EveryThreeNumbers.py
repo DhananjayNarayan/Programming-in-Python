2c63300c826b73e401373af92291b26a74adf616
Dhananjay Narayan <dhananjayashwath@gmail.com>
2020-05-23 22:22:22 +0530

    Create EveryThreeNumbers.py

---
+++ CodeCademy Challenges/EveryThreeNumbers.py
@@ -0,0 +1,21 @@
+"""
+Create a function called every_three_nums that has one parameter named start.
+
+The function should return a list of every third number between start and 100 (inclusive). 
+For example, every_three_nums(91) should return the list [91, 94, 97, 100]. 
+If start is greater than 100, the function should return an empty list.
+"""
+
+def every_three_nums(start):
+  lst=[]
+  
+  if(start>100):
+    return lst
+  
+  for i in range(start,101,3):
+    lst.append(i)
+    
+  return lst
+
+
+print(every_three_nums(91))
