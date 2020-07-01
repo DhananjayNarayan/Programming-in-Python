fa08028e61036f40501ab3ff86bdeabb816c8e49
Dhananjay Narayan <dhananjayashwath@gmail.com>
2020-07-01 22:39:39 +0530

    Create PY4EAssignment7_2.py

---
+++ Python For Everybody (Coursera)/PY4EAssignment7_2.py
@@ -0,0 +1,23 @@
+"""
+Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+X-DSPAM-Confidence:    0.8475
+Count these lines and extract the floating point values from each of the lines and compute the average of those values and produce an output as shown below.
+Do not use the sum() function or a variable named sum in your solution.
+You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
+"""
+
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+add = 0
+count = 0
+for line in fh:
+    if (line.startswith("X-DSPAM-Confidence:")):
+        count += 1
+        start = line.find(" ")
+        line = line[start:]
+        textInNumber = float(line.strip())
+        add += textInNumber
+
+
+print("Average spam confidence:", add / count)
