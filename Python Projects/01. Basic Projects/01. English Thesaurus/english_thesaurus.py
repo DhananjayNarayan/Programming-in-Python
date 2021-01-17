90d92ef1020ad2401f869e551d3f9014721df099
Dhananjay Narayan <dhananjayashwath@gmail.com>
2021-01-17 15:24:17 +0530

    Update english_thesaurus.py

--- Python Projects/01. Basic Projects/01. English Thesaurus/english_thesaurus.py
+++ Python Projects/01. Basic Projects/01. English Thesaurus/english_thesaurus.py
@@ -9,7 +9,14 @@ def fetch(word):
     if word in data:
         return data[word]
     elif len(get_close_matches(word, data.keys())) > 0:
-        return "Incorrect Word. Do you mean %s instead?" % get_close_matches(word, data.keys())[0]
+        yn = input("Incorrect Word. Do you mean %s instead? Enter 'Y' for Yes, 'N' for No : " %
+                   get_close_matches(word, data.keys())[0])
+        if yn == 'Y' or yn == 'y':
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == 'N' or yn == 'n':
+            return "The word doesn't exist. Please check or enter different a word."
+        else:
+            return "Incorrect Entry."
     else:
         return "The word doesn't exist. Please check or enter different a word."
 
