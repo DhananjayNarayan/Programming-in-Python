456cb2f2884aa49239856ba3d5baad02bad6ee8e
Dhananjay Narayan <dhananjayashwath@gmail.com>
2021-01-17 14:57:36 +0530

    Update english_thesaurus.py

--- Python Projects/01. Basic Projects/01. English Thesaurus/english_thesaurus.py
+++ Python Projects/01. Basic Projects/01. English Thesaurus/english_thesaurus.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -7,8 +8,10 @@ def fetch(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Incorrect Word. Do you mean %s instead?" % get_close_matches(word, data.keys())[0]
     else:
-        return "The word doesn't exist. Please check and enter different a word."
+        return "The word doesn't exist. Please check or enter different a word."
 
 
 word = input("Enter a Word: ")
